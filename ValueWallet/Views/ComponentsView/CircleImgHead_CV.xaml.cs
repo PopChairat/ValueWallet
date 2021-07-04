2485bbdc6c855d7d30c109ab0c0e099c4aad6be7
PopChairat <75405911+PopChairat@users.noreply.github.com>
2021-07-04 19:39:23 +0700

    Add Component Circle Img head

---
+++ ValueWallet/Views/ComponentsView/CircleImgHead_CV.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using ValueWallet.Models;
+using Xamarin.Forms;
+
+namespace ValueWallet.Views.ComponentsView
+{
+    public partial class CircleImgHead_CV : ContentView
+    {
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly BindableProperty TextProperty =
+                            BindableProperty.Create(
+                        propertyName:nameof(Text),
+                        returnType: typeof(string),
+                        declaringType: typeof(CircleImgHead_CV),
+                        defaultBindingMode: BindingMode.OneWay);
+
+        public CircleImgHead_CV()
+        {
+            InitializeComponent();
+
+            if (LocalDeviceInfo.CurrentDevice.IsNotchIosDevice)
+                NotchBx.IsVisible = true;
+            else
+                NotchBx.IsVisible = false;
+
+            BackgroundColor = (Color)Application.Current.Resources["BlueBackground"];
+
+            BindingContext = this;
+        }
+    }
+}
---
+++ ValueWallet/Views/ComponentsView/CloseNavigation_CV.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ValueWallet.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ValueWallet.Views.ComponentsView
+{
+    public partial class CloseNavigation_CV : ContentView
+    {
+        public event EventHandler Clicked;
+
+        public CloseNavigation_CV()
+        {
+            InitializeComponent();
+
+            if (LocalDeviceInfo.CurrentDevice.IsNotchIosDevice)
+                NotchBx.IsVisible = true;
+            else
+                NotchBx.IsVisible = false;
+
+            BindingContext = this;
+        }
+
+        void Close_Tapped(Object sender, EventArgs e)
+        {
+            if(Clicked != null)
+            {
+                Clicked(this, new EventArgs());
+            }
+            else
+            {
+                if (Navigation.ModalStack.Count > 0)
+                {
+                    Navigation.PopModalAsync();
+                }
+                else if (Navigation.NavigationStack.Count > 0)
+                {
+                    Navigation.PopAsync();
+                }
+            }
+        }
+    }
+}
